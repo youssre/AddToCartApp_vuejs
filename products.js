7678a1123c61d9ce019546c7f8a8e893e80d484c
youssre <youssre092@gmail.com>
2022-02-02 20:37:40 +0200

    first commit by yousri

---
+++ products.js
@@ -0,0 +1,53 @@
+products=[
+    {
+        id:100,
+        name:'product One',
+        price:1000,
+        instock:5,
+        description:"product One is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+        image:'https://placekitten.com/250/250'
+    },
+    {
+        id:200,
+        name:'product Two',
+        price:2000,
+        instock:15,
+        description:"product Two is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+        image:'https://via.placeholder.com/250'
+    },
+    {
+        id:300,
+        name:'product Three',
+        price:3000,
+        instock:500,
+        description:"product Three is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+        image:'https://picsum.photos/250/250'
+    },
+    {
+        id:400,
+        name:'product Four',
+        price:4000,
+        instock:800,
+        description:"product Four is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+        image:'https://via.placeholder.com/250'
+    },
+    {
+        id:500,
+        name:'product Five',
+        price:5000,
+        instock:0,
+        description:"product Five is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+        image:'https://via.placeholder.com/250'
+    },
+    {
+        id:600,
+        name:'product Six',
+        price:6000,
+        instock:3,
+        description:"product Six is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+        image:'https://via.placeholder.com/250'
+    }
+
+
+
+];
